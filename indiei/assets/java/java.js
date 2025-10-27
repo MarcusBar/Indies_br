13d44a796722042243fa27c39dd79e32e13b890f
Marcus Vinicius <marcus.alberes@gmail.com>
2025-10-27 02:55:20 -0300

    pagina de musica quase completa

--- indiei/assets/java/java.js
+++ indiei/assets/java/java.js
@@ -1,7 +1,10 @@
-function mudarLetra(id_da_div, div_antes){
+function mudarLetra(id_da_div, div_antesUm, div_antesDois){
     let div_escolhida = document.querySelector(id_da_div)
-    let div_anterior = document.querySelector(div_antes)
+    let div_anteriorUm = document.querySelector(div_antesUm)
+    let div_anteriorDois = document.querySelector(div_antesDois)
     div_escolhida.style.display = "flex";
-    div_anterior.style.display = "none";
+    div_anteriorUm.style.display = "none";
+    div_anteriorDois.style.display = "none";
+
 
 }
