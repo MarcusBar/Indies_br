48deb7e5ed2d99a566f9020c0d57feba33cf3480
Marcus Vinicius <marcus.alberes@gmail.com>
2025-10-29 17:40:23 -0300

    html e css quase prontos

--- indiei/assets/java/java.js
+++ indiei/assets/java/java.js
@@ -1,3 +1,11 @@
+document.addEventListener('click', (e) => {
+    let el = e.target;
+
+    if(el.classList.contains('home')) {
+        window.location.href = "index.html"
+    }
+})
+
 function mudarLetra(id_da_div, div_antesUm, div_antesDois){
     let div_escolhida = document.querySelector(id_da_div)
     let div_anteriorUm = document.querySelector(div_antesUm)
@@ -5,6 +13,4 @@ function mudarLetra(id_da_div, div_antesUm, div_antesDois){
     div_escolhida.style.display = "flex";
     div_anteriorUm.style.display = "none";
     div_anteriorDois.style.display = "none";
-
-
 }
