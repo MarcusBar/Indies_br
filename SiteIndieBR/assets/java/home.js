8adbc8117f5167cb57ce70b0912b7f53f9402c80
Marcus Vinicius <marcus.alberes@gmail.com>
2025-12-12 16:30:52 -0300

    Site Completo

--- SiteIndieBR/assets/java/home.js
+++ SiteIndieBR/assets/java/home.js
@@ -4,16 +4,49 @@ document.addEventListener('click', (e) => {
     if(el.id === 'historiaParaCacadores'){
         window.location.href = "historiaParaCacadores.html";
     }
+
+    if(el.id === 'tempoDePipa'){
+        window.location.href = "tempoDePipa.html";
+    }
+
+    if(el.id === 'praticarATeimosia'){
+        window.location.href = "praticarTeimosia.html";
+    }
 })
 
 var btnHome = document.getElementById('btn-home');
 var bntSobreNos = document.getElementById('btn-sobrenos');
 var bntNovidades = document.getElementById('btn-novidades');
 var bntMusica = document.getElementById('btn-musica');
+var bntExplore = document.getElementById('btn-explore')
+
+bntExplore.addEventListener('click', () =>{
+
+    document.getElementById('parteDaMusica').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+})
 
 bntMusica.addEventListener('click', () =>{
 
     document.getElementById('parteDaMusica').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+})
+
+bntSobreNos.addEventListener('click', () =>{
+
+    document.getElementById('sobreNos').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+})
+
+bntNovidades.addEventListener('click', () =>{
+
+    document.getElementById('novidades').scrollIntoView({
         behavior: 'smooth',
         block: 'start'
     })
